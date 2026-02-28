7bc88d36f6bc7f2062e5fecd2a8523090d778a79
David Wagner <david@wagnerkc.com>
2026-02-28 18:35:26 +0000

    Orig

--- cosmos_curate/docker/generate_dockerfile.sh
+++ cosmos_curate/docker/generate_dockerfile.sh
@@ -1,8 +1,11 @@
+# drop cuml,legacy-transformers from default envs built
+CWD=$(pwd)
 REPO_ROOT=$HOME/git/cosmos-curate
+cd $REPO_ROOT
 cosmos-curate image build \
   --curator-path "${REPO_ROOT}" \
   --image-name cosmos-curate \
   --image-tag 1 \
   --dry-run \
-  --verbose \
-  --dockerfile-output-path "${REPO_ROOT}/docker/cosmos-curate.Dockerfile"
+  --envs transformers,unified \
+  --dockerfile-output-path "${CWD}/cosmos-curate.Dockerfile"
